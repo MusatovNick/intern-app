4db8f2964bc45ae0d3a3dfc28d11af2126bfe2c5
vlad.borsh <vladborsh95s322@gmail.com>
2019-04-16 21:37:48 +0300

    add swagger

--- apps/intern-api/src/main.ts
+++ apps/intern-api/src/main.ts
@@ -4,8 +4,8 @@
  **/
 
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +13,16 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
   const port = process.env.port || 3333;
+
+  const options = new DocumentBuilder()
+    .setTitle('Intern')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('intern')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/${globalPrefix}`);
   });
