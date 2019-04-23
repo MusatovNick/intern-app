75545173c99b3807670277c2b9d9108d3fc083e4
vlad.borsh <vladborsh95s322@gmail.com>
2019-04-23 17:49:54 +0300

    replace code panel component

--- apps/intern/src/app/shared/shared.module.ts
+++ apps/intern/src/app/shared/shared.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LoaderComponent } from './loader/loader.component';
+import { CodePanelComponent } from './code-panel/code-panel.component';
+
+const COMPONENTS = [
+  HeaderComponent,
+  LoaderComponent,
+  CodePanelComponent,
+]
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    LoaderComponent,
+    ...COMPONENTS
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent,
-    LoaderComponent,
+    ...COMPONENTS
   ],
 })
 export class SharedModule { }
--- apps/intern/src/app/task/task.module.ts
+++ apps/intern/src/app/task/task.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CodePanelComponent } from './components/code-panel/code-panel.component';
 import { MonacoEditorModule } from 'ngx-monaco';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    CodePanelComponent,
   ],
   imports: [
     CommonModule,
@@ -14,7 +12,6 @@ import { SharedModule } from '../shared/shared.module';
     MonacoEditorModule,
   ],
   exports: [
-    CodePanelComponent,
   ],
 })
 export class TaskModule { }
